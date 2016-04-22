18d6c21a6206cfc7caa4a1f0799db2e83c1d351b
Paul Arizpe <paul.arizpe@arteika>
2016-04-22 14:34:46 -0500

    FizzBuzz Final

--- SuperSecret/fizzbuz.exs
+++ SuperSecret/fizzbuz.exs
@@ -1,34 +1,19 @@
 defmodule FizzBuzz do
     def solve(nums) do
         Enum.map(nums, fn(num) ->
-           cond do
-              rem(num,3) === 0 and rem(num,5) === 0 ->
-                "FizzBuzz"
-              rem(num,3) === 0
-                "Fizz"
-              rem(num,5) === 0
-                "Buzz"
-              :else ->
-                 num
-           end
-        end)
-    end
-    
-    def solve2(nums) do
-        Enum.map(nums, fn(num) ->
-           isFizz = rem(num,3) === 0
-           isBuzz = rem(num,5) === 0
+           isFizz = rem(num,3) == 0
+           isBuzz = rem(num,5) == 0
            isFizzBuzz = (isFizz and isBuzz)
            if isFizzBuzz do
-              "FizzBuzz"
+              IO.puts("FizzBuzz")
            else 
               if isFizz do
-                 "Fizz"
+                 IO.puts("Fizz")
               else 
                  if isBuzz do
-                   "Buzz"
+                   IO.puts "Buzz"
                  else
-                    num
+                   IO.puts num
                  end
               end
            end
