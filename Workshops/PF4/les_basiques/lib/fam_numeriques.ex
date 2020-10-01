3273ef851010624bc73e14430b038a04007dba9b
kiramishima <kiramishima@outlook.com>
2020-09-30 23:35:49 -0500

    Numeros

---
+++ Workshops/PF4/les_basiques/lib/fam_numeriques.ex
@@ -0,0 +1,19 @@
+defmodule FamNumeriques do
+  def numeriques() do
+    IO.puts 1
+    IO.puts(3.151416)
+  end
+
+  def sum(a, b) do
+    a + b
+  end
+
+  def diff(a, b) do
+    a - b
+  end
+
+  def division(a, b), do: a/b
+
+  def multiply(a, b), do: a*b
+
+end
