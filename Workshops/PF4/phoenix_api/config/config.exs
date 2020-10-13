b44eca958631d402d77f595ddb799fc9d987723d
kiramishima <kiramishima@outlook.com>
2020-10-12 23:54:45 -0500

    Guardian para JWT

--- Workshops/PF4/phoenix_api/config/config.exs
+++ Workshops/PF4/phoenix_api/config/config.exs
@@ -23,6 +23,13 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :auth, Auth.Guardian,
+  issuer: "Auth",
+  secret_key: "use mix phx.gen.secret",
+  permissions: %{
+    default: [:read_users, :write_users]
+  }
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
--- Workshops/PF4/phoenix_api/lib/phoenix_api_web/controllers/auth/login_controller.ex
+++ Workshops/PF4/phoenix_api/lib/phoenix_api_web/controllers/auth/login_controller.ex
@@ -2,11 +2,12 @@ defmodule PhoenixApiWeb.Auth.LoginController do
   use PhoenixApiWeb, :controller
   alias PhoenixApiWeb.Auth.Auth
 
-  /// todo
   def login(conn, %{"email" => email, "password" => password}) do
     try do
       user_count = Auth.check_if_user_exist(email, username)
       if user_count > 0 do
+        {:ok, jwt, _full_claims} =
+          Auth.Guardian.encode_and_sign(user, %{}, permissions: user.permissions)
         jwt = "token :D"
         conn |> put_status(401) |> json(%{:message => "Ya existe un usuario con el mismo email o username"})
       else
